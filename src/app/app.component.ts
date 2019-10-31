0e2bc2f85ead19b0023082e0e871d1214b8721ce
kovalenko <kovalenkogitacc@gmail.com>
2019-10-31 15:28:25 +0200

    angular watch test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'pasha-test';
+  title = 'test';
+
+  started = false;
+  currentTime = new Date(0);
+  subscription;
+
+  waitClicks = 0;
+
+  constructor() { }
+
+  startStop() {
+    this.started = !this.started;
+
+    if (!this.subscription) {
+      this.initSubscription();
+    }
+  }
+
+  reset() {
+    this.started = false;
+    this.currentTime = new Date(0);
+  }
+
+  initSubscription() {
+    this.subscription = interval(1000).subscribe(val => {
+      if (!this.started) return;
+
+      this.currentTime = new Date(this.currentTime.getTime());
+      this.currentTime.setSeconds(this.currentTime.getSeconds() + 1);
+    });
+  }
+
+  wait() {
+    this.waitClicks++;
+    if (this.waitClicks >= 2) {
+      this.started = false;
+      this.waitClicks = 0;
+    }
+
+    setTimeout(() => {
+      this.waitClicks = 0;
+    }, 300);
+  }
 }
